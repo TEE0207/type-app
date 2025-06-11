f4b2246e18546a9e739bf1d7f3b083e59f3f3dd0
Taofeek Lawal <taofeeklwl@gmail.com>
2025-06-11 22:39:23 +0100

    handling input types

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
 import { useState } from "react";
 import "./App.css"
 import InputField from "./Components/InputField";
+import { Todo } from "./Components/model";
 
 
 const App : React.FC = () => {
 
  const [todo , setTodo] = useState <string>("")
+ const [todos , setTodos] = useState <Todo[]>([]) // Todo is imported from model.ts because that's where we describe what is going to be in the array
+
+ const handleAdd = (e : React.FormEvent) => {
+   e.preventDefault()
+ }
 
   return (
     <div className="App">
       <span className="heading">Taskify</span>
 
-      <InputField todo = {todo} setTodo = {setTodo} />
+      <InputField todo = {todo} setTodo = {setTodo} handleAdd = {handleAdd} />
     </div>
   );
 }
